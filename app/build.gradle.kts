6c0cca8af133ce5c4e8017fe294fada16c6e25bd
liamtsau <liamtsau.m@gmail.com>
2026-09-26 11:31:34 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "organic.swarm.home"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "organic.swarm.home"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
